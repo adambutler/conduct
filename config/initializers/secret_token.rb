12e3285e6f897aa663a2b42c411a60fc7b764676
Adam Butler <adam@littlebigmedia.co.uk>
2013-11-06 21:19:56 +0000

    Initiated new rails 4 project conduct with psql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Conduct::Application.config.secret_key_base = 'd857ad12298de6122fe69129ef7534bc6c38449a9495b5780af82afb088f83c7bc0c40624a9640671197f2a888f528838ece750aa526295be02c6ab9646705c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Conduct::Application.config.session_store :cookie_store, key: '_conduct_session'
