1af5cd8f006bb1ba6b5326a0f217c5e5df0fa77f
Adam Butler <adam@littlebigmedia.co.uk>
2013-11-06 21:40:43 +0000

    Configure app_generators

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module Conduct
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.app_generators do |g|
+      g.test_framework :rspec, views: false, view_specs: false, helpers: false, helper_specs: false, routing_specs: false
+      g.stylesheets false
+      g.helper false
+      g.javascripts false
+    end
   end
 end
