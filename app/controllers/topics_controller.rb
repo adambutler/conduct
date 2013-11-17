eb621696de1dbc49be57d2e7f7b5e64583012f72
Adam Butler <adam@littlebigmedia.co.uk>
2013-11-17 10:43:18 +0000

    Find topic by secret
    ^ This needs some fixing since we are giving it a symbol of :id this should really be :secret but I didn't want to make the routes nasty.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -64,7 +64,7 @@ class TopicsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
-      @topic = Topic.find(params[:id])
+      @topic = Topic.find_by_secret(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
