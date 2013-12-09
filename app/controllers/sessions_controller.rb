95e834dae93a877a30747953b463d6e7b12f74f4
Adam Butler <adam@littlebigmedia.co.uk>
2013-12-09 22:45:27 +0000

    After login go to the topics_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     respond_to do |format|
       if user.authenticate(params[:user][:password])
         session[:user_id] = user.id
-        format.html { redirect_to root_url, notice: "Logged in!" }
+        format.html { redirect_to topics_path, notice: "Logged in!" }
         format.json { render :json => user }
       else
         flash.now.alert = "Uh oh... looks like your credentials aren't legit!"
