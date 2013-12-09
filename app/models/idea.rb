596e3cbcda500a26656e7f0142aca6a1d30cdd32
Adam Butler <adam@littlebigmedia.co.uk>
2013-12-09 22:42:45 +0000

    Fix paste error I set idea belongs_to :idea o_O

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ActiveRecord::Base
 
   has_many :votes
-  belongs_to :idea, foreign_key: "secret"
+  belongs_to :topic, foreign_key: "secret"
 
   def self.qty_on(date)
     where("date(created_at) = ?",date).count
