051235d221e3bb7c9071bb074777b360543946a5
Adam Butler <adam@littlebigmedia.co.uk>
2014-01-14 23:15:39 +0000

    Indentation removal

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -62,13 +62,14 @@ class TopicsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_topic
-      @topic = Topic.find_by_secret(params[:id])
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_topic
+    @topic = Topic.find_by_secret(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def topic_params
-      params.require(:topic).permit(:title, :description, :locked, :user_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def topic_params
+    params.require(:topic).permit(:title, :description, :locked, :user_id)
+  end
 end
